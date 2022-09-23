c77e1ed2696bc5ea37bf0621248b8cd1c83a4c7d
hongjungeun <gjhong1129@gmail.com>
2022-09-23 13:48:37 +0900

    fix(/internal/notion/model): NotionGetTasksResponse 수정
    ## NotionGetTasksResponse 수정
    
     - notion_response
    	 -
    NotionGetTasksResponse 수정
    		 기존 익명 struct를 NotionGetTaskResultItem으로 추출

--- internal/notion/model/notion_response.go
+++ internal/notion/model/notion_response.go
@@ -11,38 +11,40 @@ type NotionEnqueueTaskResponse struct {
 }
 
 type NotionGetTasksResponse struct {
-	Results []struct {
-		Id        string `json:"id"`
+	Results []NotionGetTaskResultItem `json:"results"`
+}
+
+type NotionGetTaskResultItem struct {
+	Id        string `json:"id"`
+	EventName string `json:"eventName"`
+	Request   struct {
+		Block struct {
+			Id string `json:"id"`
+		} `json:"block"`
+		Recursive     bool `json:"recursive"`
+		ExportOptions struct {
+			ExportType string `json:"exportType"`
+			TimeZone   string `json:"timeZone"`
+		} `json:"exportOptions"`
+	} `json:"request"`
+	Actor struct {
+		Table string `json:"table"`
+		Id    string `json:"id"`
+	} `json:"actor"`
+	State       string `json:"state"`
+	RootRequest struct {
 		EventName string `json:"eventName"`
-		Request   struct {
-			Block struct {
-				Id string `json:"id"`
-			} `json:"block"`
-			Recursive     bool `json:"recursive"`
-			ExportOptions struct {
-				ExportType string `json:"exportType"`
-				TimeZone   string `json:"timeZone"`
-			} `json:"exportOptions"`
-		} `json:"request"`
-		Actor struct {
-			Table string `json:"table"`
-			Id    string `json:"id"`
-		} `json:"actor"`
-		State       string `json:"state"`
-		RootRequest struct {
-			EventName string `json:"eventName"`
-			RequestId string `json:"requestId"`
-		} `json:"rootRequest"`
-		Headers struct {
-			Ip string `json:"ip"`
-		} `json:"headers"`
-		Status struct {
-			Type          string `json:"type"`
-			PagesExported int    `json:"pagesExported"`
-			ExportURL     string `json:"exportURL"`
-		} `json:"status"`
-		Error string `json:"error"`
-	} `json:"results"`
+		RequestId string `json:"requestId"`
+	} `json:"rootRequest"`
+	Headers struct {
+		Ip string `json:"ip"`
+	} `json:"headers"`
+	Status struct {
+		Type          string `json:"type"`
+		PagesExported int    `json:"pagesExported"`
+		ExportURL     string `json:"exportURL"`
+	} `json:"status"`
+	Error string `json:"error"`
 }
 
 func (res NotionEnqueueTaskResponse) HasErrors() bool {
