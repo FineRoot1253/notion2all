3afd55f59bc68155cd8859a0b773c2bcf7f195f7
hongjungeun <gjhong1129@gmail.com>
2022-09-23 13:55:15 +0900

    feat(/internal/notion): notion 서비스 테스트 코드 수정 및 NewService 테스트 코드 추가
    ## notion 서비스 테스트 코드 수정 및 NewService 테스트 코드 추가
    
     - notion_service_test
    	 - TestNewService 추가
    		 추후 인수테스트를 위해 추가
    	 - Test_unzip 수정
    		 잘못된 더미데이터 수정

--- internal/notion/notion_service_test.go
+++ internal/notion/notion_service_test.go
@@ -1,12 +1,65 @@
 package notion
 
 import (
+	"context"
+	"github.com/fineroot1253/notion2all/internal/common/logger"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 	"testing"
 )
+
+var commonService Service
+
+func TestNewService(t *testing.T) {
+	type args struct {
+		ctx    context.Context
+		token  string
+		logger logger.LogTemplate
+	}
+	ctx := context.Background()
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "notion NewService:[success]",
+			args: args{
+				ctx:    ctx,
+				token:  "./testdata/data/testzip/",
+				logger: logger.NewLogTemplate(logger.CommonLogRunner{}),
+			},
+			wantErr: false,
+		},
+		{
+			name: "notion NewService:[failure](모든 필드 생략)",
+			args: args{
+				ctx:    ctx,
+				token:  "./testdata/data/testzip/",
+				logger: logger.NewLogTemplate(logger.CommonLogRunner{}),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			serv, err := NewService(tt.args.ctx, tt.args.token, tt.args.logger)
+			if err != nil {
+				if tt.wantErr {
+					log.Println(err)
+				} else {
+					log.Panicln(err)
+				}
+			} else {
+				log.Println("NewService complete")
+				commonService = serv
+			}
+		})
+	}
+}
 
 func Test_unzip(t *testing.T) {
 	type args struct {
@@ -40,7 +93,7 @@ func Test_unzip(t *testing.T) {
 				src:         "",
 				destDirPath: "./testdata/data/testzip/",
 			},
-			wantErr: false,
+			wantErr: true,
 		},
 		{
 			name: "zip 파일 해제 테스트:[failure] (destDirPath 필드 생략)",
@@ -48,7 +101,7 @@ func Test_unzip(t *testing.T) {
 				src:         "./testdata/testzip.zip",
 				destDirPath: "",
 			},
-			wantErr: false,
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
@@ -72,13 +125,13 @@ func Test_unzip(t *testing.T) {
 				log.Println(tt.name, " Complete")
 			}
 			// 2차 성공 테스트를 위해 삭제
-			if tt.name == "zip 파일 해제 테스트:[success]" {
-				if err := os.RemoveAll("/testdata/data"); err != nil {
+			if strings.Contains(tt.name, "success") {
+				if err := os.RemoveAll("./testdata/data"); err != nil {
 					log.Panicln(err)
 				}
 			} else {
 				// 2차 성공 테스트 이후 반복 테스트를 위해 생성
-				if err := os.MkdirAll("/testdata/data", 0755); err != nil {
+				if err := os.MkdirAll("./testdata/data", 0755); err != nil {
 					log.Panicln(err)
 				}
 			}
