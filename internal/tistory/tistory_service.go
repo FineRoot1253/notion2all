1a1cf101f2298a86d0962980c5cb5d33e50b9c83
hongjungeun <gjhong1129@gmail.com>
2022-09-30 00:29:03 +0900

    feat(~/go/src/github.com/fineroot1253/notion2all/internal/tistory): tistory 서비스 추가
    ## tistory 서비스 추가
    
     - tistory_service
    	 - service
    		 tistory 서비스 구조체 추가
    	 - Service
    		 tistory 서비스 인터페이스 추가
    	 - NewService
    		 tistory 서비스 생성 함수 추가
    	 - GetBlogName
    		 config 구조체중 blogname을 반환하는 메서드 추가
    	 - SendPost
    		 TistoryAPI 서비스중 WritePost 메서드를 활용한 포스트 작성 메서드 추가
    	 - GetCategoryList
    		 TistoryAPI 서비스중 GetCategoryList 메서드를 활용한 카테고리 조회 메서드 추가

--- internal/tistory/tistory_service.go
+++ internal/tistory/tistory_service.go
@@ -1,1 +1,54 @@
 package tistory
+
+import (
+	"github.com/fineroot1253/notion2all/internal/common"
+	"github.com/fineroot1253/notion2all/internal/tistory/model"
+	"github.com/fineroot1253/tistoryAPI"
+	tistoryAPImodel "github.com/fineroot1253/tistoryAPI/model"
+)
+
+type service struct {
+	apiService tistoryAPI.Service
+	config     model.TistoryConfiguration
+}
+
+type Service interface {
+	GetBlogName() string
+	SendPost(data tistoryAPImodel.PostData) (tistoryAPImodel.PostWriteResult, error)
+	GetCategoryList() ([]tistoryAPImodel.CategoryData, error)
+}
+
+func NewService(config model.TistoryConfiguration, apiService tistoryAPI.Service) Service {
+	return service{config: config, apiService: apiService}
+}
+
+func (s service) GetBlogName() string {
+	return s.config.Tistory.BlogName
+}
+
+func (s service) SendPost(data tistoryAPImodel.PostData) (tistoryAPImodel.PostWriteResult, error) {
+	result, err := s.apiService.WritePost(data)
+	if err != nil {
+		return tistoryAPImodel.PostWriteResult{}, &common.CommonError{
+			Func: "SendPost",
+			Data: common.TistoryAPIRequest_Error.String(),
+			Err:  err,
+		}
+	}
+
+	return result, nil
+}
+
+func (s service) GetCategoryList() ([]tistoryAPImodel.CategoryData, error) {
+
+	list, err := s.apiService.GetCategoryList(s.GetBlogName())
+	if err != nil {
+		return nil, &common.CommonError{
+			Func: "GetCategoryList",
+			Data: common.TistoryAPIRequest_Error.String(),
+			Err:  err,
+		}
+	}
+	return list.Item.Categories, nil
+
+}
