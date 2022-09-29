ccd846a2e837873706edfa3ab862bb38b52ea4ec
hongjungeun <gjhong1129@gmail.com>
2022-09-30 00:52:59 +0900

    feat(~/go/src/github.com/fineroot1253/notion2all/internal/notion): notion 서비스 수정 및 기능 추가
    ## notion 서비스 수정 및 기능 추가
    
     - notion_service
    	 - service
    		 tokenV2 제거 후 notion config 구조체 추가
    	 - NewService
    		 notion config 구조체를 주입 받도록 변경
    	 - GetDeployPostList
    		 배포할 포스트 리스트를 반환하는 메서드 추가
    	 - GetPostHtmlDataList
    		 배포할 포스트 리스트를 토대로 html파일을 다운 받는 메서드 추가
    	 - enqueueExportTask
    		 공통 에러 구조체를 반환하도록 수정
    	 - sendPostRequest
    		 공통 에러 구조체를 반환하도록 수정
    	 - sendGetRequest
    		 공통 에러 구조체를 반환하도록 수정
    	 - unzip
    		 공통 에러 구조체를 반환하도록 수정
    	 - extractAndWriteFile
    		 공통 에러 구조체를 반환하도록 수정

--- internal/notion/notion_service.go
+++ internal/notion/notion_service.go
@@ -10,6 +10,7 @@ import (
 	"github.com/fineroot1253/notion2all/cmd/config"
 	"github.com/fineroot1253/notion2all/internal/common"
 	"github.com/fineroot1253/notion2all/internal/common/logger"
+	"github.com/fineroot1253/notion2all/internal/common/utils"
 	"github.com/fineroot1253/notion2all/internal/notion/model"
 	"github.com/kjk/notionapi"
 	"io"
@@ -17,17 +18,23 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"regexp"
 	"strings"
+	"time"
 )
 
 type Service interface {
 	/*
-		GetDeployPostList 최종 배포용 데이터 가져오기
-		@Param string // export할 페이지ID
-		@Return string, error // 다운 받은 html 파일 path 리스트, error
+		GetDeployPostList 배포 포스트 리스트 받기
+		@Return []model.NotionPostItem, error // 다운 받은 html 파일 path 리스트, error
 	*/
-	GetDeployPostList(tablePagePath string) ([]model.NotionPostData, error)
+	GetDeployPostList() ([]model.NotionPostItem, error)
+
+	/*
+		GetPostHtmlDataList 배포할 포스트의 html byte 데이터 리스트
+		@Param []model.ExportTask // 배포할 포스트데이터 리스트
+		@Return []model.NotionPostData, []model.ExportTask, error // 다운 받은 html 파일 data 리스트, 실제 수행된 html Export 요청 리스트, error
+	*/
+	GetPostHtmlDataList(postItemList []model.NotionPostItem) ([]model.ExportTask, error)
 
 	/*	enqueueExportTask Html파일 export 링크 생성 요청
 		@Param string // export할 페이지ID
@@ -45,84 +52,125 @@ type Service interface {
 		@Param []string // html 다운로드 url 리스트
 		@Return []string, error //  다운받은 파일 리스트, error
 	*/
-	downloadHtmlFiles(taskList []model.ExportTask) error
+	downloadHtmlFiles(taskList []model.ExportTask) ([]model.ExportTask, error)
 }
 
 type service struct {
 	context      context.Context
-	tokenV2      string
 	notionClient notionapi.Client
+	notionConfig model.NotionConfiguration
 	logger       logger.LogTemplate
 }
 
-func NewService(ctx context.Context, token string, logger logger.LogTemplate) (Service, error) {
-	if ctx == nil || token == "" || logger.IsEmpty() {
+func NewService(ctx context.Context, notionConfig model.NotionConfiguration, logger logger.LogTemplate) (Service, error) {
+	if ctx == nil || logger.IsEmpty() || notionConfig.IsEmpty() {
 		return nil, &common.CommonError{
 			Func: "NewService",
 			Data: common.Arguments_Error.String(),
 			Err:  errors.New("파라미터 개수가 모자릅니다"),
 		}
 	}
-	return service{context: ctx, tokenV2: token, logger: logger, notionClient: notionapi.Client{AuthToken: token}}, nil
+	return service{context: ctx, logger: logger, notionConfig: notionConfig, notionClient: notionapi.Client{AuthToken: notionConfig.Notion.Token}}, nil
 }
 
-//func (s service) GetDeployPostList(pageIdList []string) ([]model.NotionPostData, error) {
+func (s service) GetDeployPostList() ([]model.NotionPostItem, error) {
+	var spaceId string
+	var collectionId string
+	var collectionViewId string
+	var collectionKey model.NotionSchema
+	var resultData []model.NotionPostItem
+	req := new(notionapi.QueryCollectionRequest)
 
-//s.notionClient.
+	blockRecords, err := s.notionClient.DownloadPage(utils.ConvertId(s.notionConfig.GetTableBlockId()))
+	if err != nil {
+		return nil, err
+	}
+	for _, record := range blockRecords.SpaceRecords {
+		if spaceId == "" {
+			spaceId = record.Space.ID
+		}
+	}
+	for _, record := range blockRecords.CollectionViewRecords {
+		if collectionViewId == "" {
+			collectionViewId = record.CollectionView.ID
+		}
+	}
+	for _, record := range blockRecords.CollectionRecords {
+		if collectionId == "" {
+			collectionId = record.Collection.ID
+		}
 
-//return nil, nil
+		for s2, schema := range record.Collection.Schema {
+			collectionKey.SetProperty(schema.Name, s2)
+		}
+	}
 
-//// Input 레코드 , Output 레코드 초기화 로직
-//pageIdMap := map[string]model.NotionEnqueueTaskResponse{}
-//var taskIdList []string
-//var exportTask []model.ExportTask
-//
-//convertedList := convertIdList(pageIdList)
-//
-//for idx, pageId := range convertedList {
-//	result, err := s.enqueueExportTask(pageId)
-//	if err != nil {
-//		return nil, err
-//	}
-//	exportTask[idx].PageId = pageId
-//	exportTask[idx].TaskId = result.TaskId
-//	exportTask[idx].TaskResult = result
-//
-//	pageIdMap[pageId] = result
-//
-//}
-//
-//// html 파일 생성요청 상태 확인시 실패한 요청에 대해서는 확인이 불가함!
-//// 		=> taskId가 없기 때문임
-//// 이를 위해 에러 없이 성공한 요청 TaskId만 모은 taskIdList를 만들고
-//// 이를 이용해 상태확인 및 동기 처리를 시도한다.
-//for _, response := range pageIdMap {
-//	if !response.HasErrors() {
-//		taskIdList = append(taskIdList, response.TaskId)
-//	}
-//}
-//
-//// 완성할때까지 0.5초 대기후 재확인하는 동기화 로직
-//// 보통은 한번에 통과하지만 정말 큰 페이지[루트블럭]은 좀 오래걸리긴한다.
-//for {
-//	status, err := s.getTaskStatusList(taskIdList)
-//	if err != nil {
-//		return nil, err
-//	}
-//	if status.HasIncompleteTask() {
-//		time.Sleep(time.Microsecond * 500)
-//	} else {
-//		break
-//	}
-//}
+	req.CollectionView.SpaceID = spaceId
+	req.CollectionView.ID = collectionViewId
+	req.Collection.SpaceID = spaceId
+	req.Collection.ID = collectionId
 
-//return []string{}, nil
+	collection, err := s.notionClient.QueryCollection(*req, nil)
+	if err != nil {
+		return nil, err
+	}
 
-//}
+	for _, record := range collection.RecordMap.Blocks {
+		var temp model.NotionPostItem
+		for s3, i := range record.Block.Properties {
+			temp.SetProperty(collectionKey, s3, i)
+		}
+		resultData = append(resultData, temp)
+	}
 
-func (s service) GetDeployPostList(tablePagePath string) ([]model.NotionPostData, error) {
-	//s.notionClient.
-	return nil, nil
+	return resultData, nil
+}
+
+func (s service) GetPostHtmlDataList(postItemList []model.NotionPostItem) ([]model.ExportTask, error) {
+
+	var taskIdList []string
+	var exportTaskList []model.ExportTask
+
+	for _, item := range postItemList {
+		pageId := utils.ConvertPageUrlToPageId(item.Content)
+		task, _ := s.enqueueExportTask(pageId)
+		taskIdList = append(taskIdList, task.TaskId)
+		exportTaskList = append(exportTaskList, model.ExportTask{
+			PageId:     pageId,
+			TaskId:     task.TaskId,
+			TaskResult: task,
+		})
+	}
+
+	for true {
+		res, err := s.getTaskStatusList(utils.ConvertIdList(taskIdList))
+		if err != nil {
+			return nil, err
+		}
+		if res.HasIncompleteTask() {
+			time.Sleep(750 * time.Millisecond)
+		} else {
+			exportTaskList = res.GetExportDataList(exportTaskList)
+			break
+		}
+	}
+
+	exportTaskList, err := s.downloadHtmlFiles(exportTaskList)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, task := range exportTaskList {
+		destDirPath := filepath.Join(config.NOTION_HTML_DATA_PATH, task.PageId)
+		htmlFilePath, err := unzip(task.FilePath, destDirPath)
+		if err != nil {
+			return nil, err
+		}
+		task.FilePath = htmlFilePath
+	}
+
+	return exportTaskList, nil
+
 }
 
 func (s service) enqueueExportTask(pageId string) (model.NotionEnqueueTaskResponse, error) {
@@ -143,13 +191,17 @@ func (s service) enqueueExportTask(pageId string) (model.NotionEnqueueTaskRespon
 			},
 		},
 	}
-	byteData, err := sendPostRequest(s.context, request, config.NOTION_ENQUEUE_TASK_PATH, s.tokenV2)
+	byteData, err := sendPostRequest(s.context, request, config.NOTION_ENQUEUE_TASK_PATH, s.notionConfig.Notion.Token)
 	if err != nil {
 		return result, err
 	}
 
 	if err := json.Unmarshal(byteData, &result); err != nil {
-		return result, err
+		return result, &common.CommonError{
+			Func: "enqueueExportTask",
+			Data: common.Marshal_Error.String(),
+			Err:  err,
+		}
 	}
 
 	return result, nil
@@ -161,17 +213,17 @@ func (s service) getTaskStatusList(taskIdList []string) (model.NotionGetTasksRes
 	request := model.CheckRequest{
 		TaskIds: taskIdList,
 	}
-	byteData, err := sendPostRequest(s.context, request, config.NOTION_GET_TASKS_PATH, s.tokenV2)
+	byteData, err := sendPostRequest(s.context, request, config.NOTION_GET_TASKS_PATH, s.notionConfig.Notion.Token)
 	if err != nil {
 		return result, err
 	}
 
 	if err := json.Unmarshal(byteData, result); err != nil {
-		return result, err
-	}
-
-	if result.HasErrors() {
-		return result, err
+		return result, &common.CommonError{
+			Func: "getTaskStatusList",
+			Data: common.Marshal_Error.String(),
+			Err:  err,
+		}
 	}
 
 	return result, nil
@@ -196,23 +248,39 @@ func sendPostRequest[NR model.NotionRequest](ctx context.Context, request NR, pa
 
 	reqByte, err := json.Marshal(request)
 	if err != nil {
-		return nil, err
+		return nil, &common.CommonError{
+			Func: "sendPostRequest",
+			Data: common.Marshal_Error.String(),
+			Err:  err,
+		}
 	}
 	if _, err := body.Write(reqByte); err != nil {
-		return nil, err
+		return nil, &common.CommonError{
+			Func: "sendPostRequest",
+			Data: common.Unexpected_Error.String(),
+			Err:  err,
+		}
 	}
 
 	reqWithCtx, err := http.NewRequestWithContext(ctx, http.MethodPost, config.NOTION_V3_BASE_URL+path, body)
 	reqWithCtx.Header.Set("Cookie", "token_v2="+token)
 	response, err := client.Do(reqWithCtx)
 	if err != nil {
-		return nil, err
+		return nil, &common.CommonError{
+			Func: "sendPostRequest",
+			Data: common.NotionAPIRequest_Error.String(),
+			Err:  err,
+		}
 	}
 
 	defer response.Body.Close()
 	all, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return nil, err
+		return nil, &common.CommonError{
+			Func: "sendPostRequest",
+			Data: common.Unexpected_Error.String(),
+			Err:  err,
+		}
 	}
 
 	return all, nil
@@ -224,30 +292,51 @@ func sendGetRequest(ctx context.Context, url string) ([]byte, error) {
 	reqWithCtx, err := http.NewRequestWithContext(ctx, http.MethodPost, url, nil)
 	response, err := client.Do(reqWithCtx)
 	if err != nil {
-		return nil, err
+		return nil, &common.CommonError{
+			Func: "sendPostRequest",
+			Data: common.NotionAPIRequest_Error.String(),
+			Err:  err,
+		}
 	}
 
 	defer response.Body.Close()
 	all, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return nil, err
+		return nil, &common.CommonError{
+			Func: "sendPostRequest",
+			Data: common.Unexpected_Error.String(),
+			Err:  err,
+		}
 	}
 
 	return all, nil
 }
 
-func (serv service) downloadHtmlFiles(taskList []model.ExportTask) error {
+func (s service) downloadHtmlFiles(taskList []model.ExportTask) ([]model.ExportTask, error) {
 
-	if err := os.MkdirAll(".", 0755); err != nil {
-		return err
+	resultList := taskList
+
+	if err := os.MkdirAll(config.NOTION_HTML_DATA_PATH, 0755); err != nil {
+		return nil, &common.CommonError{
+			Func: "downloadHtmlFiles",
+			Data: common.Unexpected_Error.String(),
+			Err:  err,
+		}
 	}
 
-	for _, data := range taskList {
-		byteData, _ := sendGetRequest(serv.context, data.GetExportUrl())
-		file, err := os.Create(data.PageId)
+	for _, data := range resultList {
+		byteData, _ := sendGetRequest(s.context, data.GetExportUrl())
+		filePath := filepath.Join(config.NOTION_HTML_DATA_PATH, data.PageId, ".zip")
+		file, err := os.Create(filePath)
 		if err != nil {
-			return err
+			return nil, &common.CommonError{
+				Func: "downloadHtmlFiles",
+				Data: common.Unexpected_Error.String(),
+				Err:  err,
+			}
 		}
+
+		data.FilePath = filePath
 
 		defer func() {
 			file.Close()
@@ -255,61 +344,81 @@ func (serv service) downloadHtmlFiles(taskList []model.ExportTask) error {
 
 		_, err = file.Write(byteData)
 		if err != nil {
-			return err
+			return nil, &common.CommonError{
+				Func: "downloadHtmlFiles",
+				Data: common.Unexpected_Error.String(),
+				Err:  err,
+			}
 		}
-
 	}
 
-	return nil
+	return resultList, nil
 
-}
-
-/*	convertIdList pageid 리스트를 uuid-v4 형식으로 변환
-	@Param	[]string // pageId 리스트
-	@Return	[]string // 변환된 pageId 리스트
-*/
-func convertIdList(idList []string) []string {
-	for _, id := range idList {
-		id = regexp.MustCompile(`(.{8})(.{4})(.{4})(.{4})(.+)`).ReplaceAllString(id, `$1-$2-$3-$4-$5`)
-	}
-	return idList
 }
 
 /*	unzip export 파일 결과인 zip파일 해제
-	@Param	[]string // pageId 리스트
-	@Return	[]string // 변환된 pageId 리스트
+	@Param	string, string // zip 파일 위치, 파일 해제 위치
+	@Return	string, error // html 파일 위치, error
 */
-func unzip(src string, destDirPath string) error {
+func unzip(src string, destDirPath string) (string, error) {
+
+	var htmlFilePath string
+
 	r, err := zip.OpenReader(src)
 	if err != nil {
-		return err
+		return "", &common.CommonError{
+			Func: "unzip",
+			Data: common.Unexpected_Error.String(),
+			Err:  err,
+		}
 	}
 	defer func() {
 		if err := r.Close(); err != nil {
-			panic(err)
+			panic(&common.CommonError{
+				Func: "extractAndWriteFile",
+				Data: common.Unexpected_Error.String(),
+				Err:  err,
+			})
 		}
 	}()
 	if err := os.MkdirAll(destDirPath, 0755); err != nil {
-		return err
+		return "", &common.CommonError{
+			Func: "unzip",
+			Data: common.Unexpected_Error.String(),
+			Err:  err,
+		}
 	}
 
 	for _, f := range r.File {
+
 		err := extractAndWriteFile(f, destDirPath)
 		if err != nil {
-			return err
+			return "", err
+		}
+		if strings.HasSuffix(f.Name, ".html") {
+			htmlFilePath = filepath.Join(destDirPath, f.Name)
 		}
 	}
-	return nil
+	return htmlFilePath, nil
 }
 
 func extractAndWriteFile(f *zip.File, destDirPath string) error {
+
 	rc, err := f.Open()
 	if err != nil {
-		return err
+		return &common.CommonError{
+			Func: "extractAndWriteFile",
+			Data: common.Unexpected_Error.String(),
+			Err:  err,
+		}
 	}
 	defer func() {
 		if err := rc.Close(); err != nil {
-			panic(err)
+			panic(&common.CommonError{
+				Func: "extractAndWriteFile",
+				Data: common.Unexpected_Error.String(),
+				Err:  err,
+			})
 		}
 	}()
 
@@ -321,45 +430,49 @@ func extractAndWriteFile(f *zip.File, destDirPath string) error {
 
 	if f.FileInfo().IsDir() {
 		if err := os.MkdirAll(path, f.Mode()); err != nil {
-			return err
+			return &common.CommonError{
+				Func: "extractAndWriteFile",
+				Data: common.Unexpected_Error.String(),
+				Err:  err,
+			}
 		}
 
 	} else {
+
 		if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
-			return err
+			return &common.CommonError{
+				Func: "extractAndWriteFile",
+				Data: common.Unexpected_Error.String(),
+				Err:  err,
+			}
 		}
 		f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
 		if err != nil {
-			return err
+			return &common.CommonError{
+				Func: "extractAndWriteFile",
+				Data: common.Unexpected_Error.String(),
+				Err:  err,
+			}
 		}
 		defer func() {
 			if err := f.Close(); err != nil {
-				panic(err)
+				panic(&common.CommonError{
+					Func: "extractAndWriteFile",
+					Data: common.Unexpected_Error.String(),
+					Err:  err,
+				})
 			}
 		}()
 
 		_, err = io.Copy(f, rc)
 		if err != nil {
-			return err
+			return &common.CommonError{
+				Func: "extractAndWriteFile",
+				Data: common.Unexpected_Error.String(),
+				Err:  err,
+			}
 		}
+
 	}
 	return nil
-}
-
-/*	replaceImgSrc unzip 결과로 파일이 1개 이상일때(이미지파일)
-	해당 img 태그 src를 base64로 치환
-	@Param	[]string // pageId 리스트
-	@Return	[]string // 변환된 pageId 리스트
-*/
-func replaceImgSrc() error {
-
-	return nil
-}
-
-/*	replaceImgSrc 이미지 파일 base64 반환
-	@Param	[]string // pageId 리스트
-	@Return	[]string // 변환된 pageId 리스트
-*/
-func getImageBase64String() (string, error) {
-	return "", nil
 }
