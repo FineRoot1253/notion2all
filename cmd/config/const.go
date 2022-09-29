56445b379043cdd5f0e5e56fba4fd4ca2c61f30f
hongjungeun <gjhong1129@gmail.com>
2022-09-29 23:40:34 +0900

    feat(~/go/src/github.com/fineroot1253/notion2all/cmd/config): const 상수 추가
    ## const 상수 추가
    
     - const
    	 - NOTION_HTML_DATA_PATH
    		 notion 서비스중 html 관련 데이터 경로 추가
    	 - NOTION_CODE_BLOCK_THEHE
    		 notion 서비스 html 파싱중 사용되는 코드블럭 타입 추가
    	 - NOTION_CSS
    		 notion 서비스 html 파싱중 사용되는 CSS 링크 추가
    	 - NOTION_STYLE_TAG
    		 notion style 태그 추가

--- cmd/config/const.go
+++ cmd/config/const.go
@@ -16,6 +16,66 @@ const (
 
 	NOTION_V3_BASE_URL = "https://www.notion.so/api/v3"
 
+	NOTION_HTML_DATA_PATH   = "./data"
+	NOTION_CODE_BLOCK_THEME = "atom-one-dark"
+
 	NOTION_ENQUEUE_TASK_PATH = "/enqueueTask"
 	NOTION_GET_TASKS_PATH    = "/getTasks"
+
+	NOTION_CSS       = "https://rawcdn.githack.com/ppuep94/n2t/5ef4dc01e9d6336341e9ab95bb71672f9d3a3dc9/assets/css/style2.css"
+	NOTION_STYLE_TAG = `<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/{code_theme}.min.css">
+				<style> 
+.bookmark {
+    text-decoration: none;
+    max-height: 8em;
+    padding: 0;
+    display: flex;
+    width: 100%;
+    align-items: stretch;
+}
+.source {
+    border: 1px solid #ddd;
+    border-radius: 3px;
+    padding: 1.5em;
+    word-break: break-all;
+}
+a, a.visited {
+    color: inherit;
+    text-decoration: underline;
+}
+.bookmark-image {
+    width: 33%;
+    flex: 1 1 180px;
+    display: block;
+    position: relative;
+    object-fit: cover;
+    border-radius: 1px;
+}
+.bookmark-info {
+    flex: 4 1 180px;
+    padding: 12px 14px 14px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+.sans {
+    font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol";
+}
+img {
+    max-width: 100%;
+}
+.page-header-icon {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+}
+.icon {
+    display: inline-block;
+    max-width: 1.2em;
+    max-height: 1.2em;
+    text-decoration: none;
+    vertical-align: text-bottom;
+    margin-right: 0.5em;
+}</style>
+                <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/highlight.min.js"></script>
+                <script>hljs.initHighlightingOnLoad();</script>`
 )
